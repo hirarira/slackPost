1de73558e3f3263288821d9396494b2e4e0f4209
hirarira <hirarira617@gmail.com>
2018-05-21 01:24:00 +0900

    Promiseを導入

--- slackTaklTest.js
+++ slackTaklTest.js
@@ -1,3 +1,13 @@
 "use strict";
 let postSlack = require("./slackPost.js");
-postSlack("こんにちは！");
+postSlack("こんにちは！")
+.then((res)=>{
+	console.log(res);
+	return postSlack("続けてこんにちは！")
+})
+.then((res)=>{
+	console.log(res);
+})
+.catch((err)=>{
+	console.log(err);
+});
