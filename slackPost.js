b25ab267d170489c26644f6a5bb6ac3b6c1f15b1
hirarira <hirarira617@gmail.com>
2019-09-22 19:04:38 +0900

    環境変数から読み込み

--- slackPost.js
+++ slackPost.js
@@ -10,10 +10,9 @@ function getToken(path){
 module.exports = (post_str) => {
 	return new Promise((resolve, reject)=>{
 		// 投稿するチャンネルID
-		let channelID = getToken("./token/channelID.txt");
-		let token = getToken("./token/token.txt");
-		let request = require('request');
-		console.log(token);
+		const channelID = process.env.SLACK_POST_CHANNNEL;
+		const token = process.env.SLACK_TOKEN;
+		const request = require('request');
 		let set_url = "https://slack.com/api/chat.postMessage?token=" + token;
 		set_url += "&channel=" + channelID + "&as_user=true&pretty=1&text=";
 		set_url += encodeURIComponent(post_str);
